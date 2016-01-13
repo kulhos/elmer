db4db3a5767677b3e2c745af61a03ed1301c1e2f
kulhos <kulhan@p3plcpnl0643.prod.phx3.secureserver.net>
2016-01-13 06:19:02 -0700

    sql

---
+++ smrz_values.sql
@@ -0,0 +1,102 @@
+-- MySQL dump 10.13  Distrib 5.5.45, for Linux (x86_64)
+--
+-- Host: localhost    Database: netfort_cz
+-- ------------------------------------------------------
+-- Server version	5.5.45-cll-lve
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `smrz_values`
+--
+
+DROP TABLE IF EXISTS `smrz_values`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `smrz_values` (
+  `time` datetime NOT NULL,
+  `sensor` int(11) NOT NULL,
+  `value` decimal(10,1) DEFAULT NULL,
+  `remove` tinyint(1) NOT NULL,
+  PRIMARY KEY (`time`,`sensor`),
+  UNIQUE KEY `uniq` (`time`,`sensor`,`value`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin2;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`kulhan`@`localhost`*/ /*!50003 TRIGGER `round_value` BEFORE INSERT ON `smrz_values` FOR EACH ROW BEGIN
+if NEW.value is NULL
+	then
+		set NEW.value = 0;
+	else
+		set NEW.value = round(NEW.value,1);
+end if;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = '' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`cpses_ku2GvSrCq2`@`localhost`*/ /*!50003 TRIGGER `AFTER_INSERT_update_current` AFTER INSERT ON `smrz_values` FOR EACH ROW BEGIN
+if (NEW.sensor > 1)
+THEN
+	insert into smrz_values_current
+	(sensor, time, value)
+	values (NEW.sensor, NEW.time, NEW.value)
+	on duplicate key update time = NEW.time, value = NEW.value;
+
+ELSEIF (NEW.sensor = 1)
+THEN
+	select value into @cval from smrz_values_current
+	where sensor = NEW.sensor;
+
+	insert into smrz_values_current
+	(sensor, time, value)
+	values (NEW.sensor, NEW.time, @cval + NEW.value)
+	on duplicate key update
+	value = @cval + NEW.value;
+
+END IF;
+
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-13  6:16:57
